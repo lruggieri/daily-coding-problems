11a6c0d7ec4cb24cd15a009c17b37e63e169396f
Luca Ruggieri <luc4.ruggieri@gmail.com>
2020-03-23 18:56:15 +0100

    problem 159

---
+++ problem159/main.go
@@ -0,0 +1,13 @@
+package problem159
+
+
+func Solution(s string) rune{
+	alreadySeen := make(map[rune]bool)
+	for _,c := range s{
+		if alreadySeen[c]{
+			return c
+		}
+		alreadySeen[c] = true
+	}
+	return -1
+}
---
+++ problem159/main_test.go
@@ -0,0 +1,12 @@
+package problem159
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	assert.Equal(t,'b',Solution("acbbac"))
+	assert.Equal(t,rune(-1),Solution("abcdef"))
+	assert.Equal(t,rune(-1),Solution(""))
+}
